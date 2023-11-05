3ddceb8e5fcc7a95a1f6bbfc481823624c128c6c
Igor Stojanovski <igor.stojanovski1996@gmail.com>
2023-11-05 19:54:39 +0100

    initial commit

---
+++ refs/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Player from "./components/Player";
+
+function App() {
+  return (
+    <main>
+      <Player />
+      <div id="challenges"></div>
+    </main>
+  );
+}
+
+export default App;
